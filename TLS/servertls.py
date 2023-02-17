9c7ed81e16ace20675d1bea175d3dbadcd5bc0af
Pouria ArefiJamal <74317596+Pouria-Empire@users.noreply.github.com>
2023-02-17 22:57:46 +0330

    Update servertls.py

--- TLS/servertls.py
+++ TLS/servertls.py
@@ -1,5 +1,6 @@
 from socket import socket, AF_INET, SOCK_STREAM,SOCK_DGRAM
 from ssl import SSLContext, PROTOCOL_TLS_SERVER
+import _thread as thread
 MAX_DATA_RECV = 4096
 
 ip = '127.0.0.1'
@@ -7,32 +8,44 @@ port = 8442
 context = SSLContext(PROTOCOL_TLS_SERVER)
 context.load_cert_chain('./TLS/new.pem', './TLS/private.key') #load tls connection
 
+def func_thread(data, connection):
+    global ip,port,context
+    # get the request from browser
+    (message,destPort) = [x.split(": ")[1] for x in data.decode('utf-8').split(",")] # get message and port from tunnel
+
+    # create socket for sending back the result
+    udpServerAppSock = socket(AF_INET,SOCK_DGRAM)
+    udpServerAppSock.sendto(message.encode('ascii'),(ip,int(destPort))) # Send udp message
+    print("waiting for response of server app...")
+
+    # recieve response and print it
+    recv_data,(ip,destPort) = udpServerAppSock.recvfrom(MAX_DATA_RECV)
+
+    # just for applications without response
+    if recv_data is not None:
+        connection.sendall(recv_data) 
+    else:
+        connection.sendall(b'Message sent without response')
+
+    print(f"response: {recv_data} sent")
+
+
 def runTLSServer():
     global ip,port,context
     #create socket and listening on
+
     with socket(AF_INET, SOCK_STREAM) as server:
         server.bind((ip, port))
         server.listen(1)
 
         with context.wrap_socket(server, server_side=True) as tls:
-            connection, address = tls.accept()  # waiting for connection
-            print(f'Connected by {address}\n')
+            while 1:
+                connection, address = tls.accept()  # waiting for connection
+                print(f'Connected by {address}\n')
 
-            data = connection.recv(1024) #recieve data
-            print(f'Client Says: {data}')
+                data = connection.recv(1024) #recieve data
+                print(f'Client Says: {data}')
 
-            (message,destPort) = [x.split(": ")[1] for x in data.decode('utf-8').split(",")] # get message and port from tunnel
+                thread.start_new_thread(func_thread, (data,connection,))
 
-            # create socket for sending back the result
-            udpServerAppSock = socket(AF_INET,SOCK_DGRAM)
-            udpServerAppSock.sendto(message.encode('ascii'),(ip,int(destPort))) # Send udp message
-            print("...waiting for response of server app...")
-
-            # recieve response and print it
-            recv_data,(ip,destPort) = udpServerAppSock.recvfrom(MAX_DATA_RECV)
-
-            # just for applications without response
-            if recv_data is not None:
-                connection.sendall(recv_data) 
-            else:
-                connection.sendall(b'Message sent without response')
+              
